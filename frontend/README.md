c98dde48168700cbf806d2604e9665d56b2aa945
Dean Chapman <p14n@Deans-MacBook-Pro.local>
2017-03-11 17:34:18 +0000

    Encrypted upload works

---
+++ frontend/README.md
@@ -0,0 +1,12 @@
+Front end app
+=============
+
+Development
+-------------
+* Install node
+* `npm install`
+* `npm run package`
+* `npm run server`
+* Open http://localhost:3000/
+* Upload a file
+* See the encrypted version in `./uploads`
--- frontend/server.js
+++ frontend/server.js
@@ -9,6 +9,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', function(req, res){
   res.sendFile(path.join(__dirname, 'dist/index.html'));
 });
+app.get('/bundle.js', function(req, res){
+  res.sendFile(path.join(__dirname, 'dist/bundle.js'));
+});
 
 app.post('/upload', function(req, res){
 

5191ff73c7e8f3e7d42b135610a8e7a95367139f
Dean Chapman <p14n@Deans-MacBook-Pro.local>
2017-03-13 21:42:12 +0000

    Add peerjs test

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,5 +1,6 @@
 import PubSub from "pubsub-js";
 import {upload} from "./uploader";
+import Peer from 'peerjs';
 
 const q = (sel) => document.querySelector(sel);
 
@@ -30,4 +31,32 @@ q('#upload-input').addEventListener('change', function(){
   upload(files);
 
 });
+
+
+
+q('#listen-button').addEventListener('click', function (){
+
+    var lname = q('#listen-input').value;
+    console.log("listen "+lname)
+    var peer = new Peer(lname,{key: 'e4ddfhot38nkx1or'});
+    peer.on('connection', function(conn) {
+      console.log("listen connected "+lname)
+      conn.on('data', function(data){
+        // Will print 'hi!' 
+        console.log(data);
+      });
+    });
+
+    q('#send-button').addEventListener('click', function (){
+
+      var name = q('#send-input').value;
+      console.log("sending to "+name)
+      var sconn = peer.connect(name);
+      sconn.on('open', function(){
+        sconn.send('hi '+name);
+      });
+    });
+});
+
+
 
